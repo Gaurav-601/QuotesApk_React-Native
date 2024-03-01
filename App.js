3d2560681c10dbb3500ccc87e7406506171fa009
Gaurav Tiwari <121477756+Gaurav-601@users.noreply.github.com>
2024-03-01 11:18:25 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import { View, Text } from "react-native";
+import React from "react";
+import AppNaviagtor from "./screens/AppNavigator";
+// import AppNaviagtor from "./screens/AppNavigator";
+
+const App = () => {
+  return (
+    // <View>
+    <AppNaviagtor />
+
+    // </View>
+  );
+};
+
+export default App;
